ae5f50200a9baa6f7751c78b8e2afb8cb2819a0a
Sakib3602 <sakibsarker6969@gmail.com>
2024-03-10 23:34:01 +0600

    added wish list

--- src/comp/Country/CardCountry/CardCountry.jsx
+++ src/comp/Country/CardCountry/CardCountry.jsx
@@ -1,36 +1,47 @@
 import { useState } from "react";
 
+const CardCountry = (props) => {
+  // let {name , capital} = e
+  let { name, capital, coatOfArms, cca3, } = props.e
 
-const CardCountry = ({e:{name , capital, coatOfArms, cca3}}) => {
+  let [visit, setVisit] = useState(false);
 
-   
-    
+  let clickk = () => {
+    setVisit(!visit);
+  };
 
-    // let {name , capital} = e
+  return (
+    <div className="">
+      <div
+        className={`${
+          visit ? "bg-red-300" : ""
+        }  border-[2px] border-solid border-[red] w-[480px] min-h-[400px] m-3 p-[10px] rounded-2xl`}
+      >
+        <img className="h-[120px]" src={coatOfArms.png} alt="" />
+        <h1 className="text-[40px]">Name: {name?.common}</h1>
+        <h1 className="text-[20px]">Capital : {capital}</h1>
+        <p>Code : {cca3}</p>
 
-    let [visit,setVisit] = useState(false)
+        <button
+          onClick={clickk}
+          className="border-[2px] border-solid border-[red] p-2 rounded-lg"
+        >
+          Visited
+        </button>
+        {visit ? "I have Visited This Place." : "I want to visit"}
+        <br />
 
-    let clickk = ()=>{
-        setVisit(!visit)
-    }
-
-
-
-    
-    return (
-       <div className="">
-         <div className={`${visit ? "bg-red-300" : ''}  border-[2px] border-solid border-[red] w-[480px] min-h-[400px] m-3 p-[10px] rounded-2xl`}>
-            <img className="h-[120px]" src={coatOfArms.png} alt="" />
-            <h1 className="text-[60px]">Name: {name?.common}</h1>
-            <h1 className="text-[30px]">Capital : {capital}</h1>
-            <p>Code : {cca3}</p>
-
-            <button onClick={clickk} className="border-[2px] border-solid border-[red] p-2 rounded-lg">Visited</button>
-            {visit ? 'I have Visited This Place.' : 'I want to visit'}
-            
-        </div>
-       </div>
-    );
+        <button
+          onClick={()=>props.visited(props.e)}
+          className="border-[2px] border-solid border-[red] p-2 rounded-lg"
+        >
+          Wish List
+        </button>
+        <br />
+        
+      </div>
+    </div>
+  );
 };
 
 export default CardCountry;
--- src/comp/Country/Country.jsx
+++ src/comp/Country/Country.jsx
@@ -13,13 +13,33 @@ const Country = () => {
         .then(data => setApi(data))
     },[])
     console.log(api)
+
+    const [ui,setUi] = useState([])
+
+    function visited(api){
+        console.log("visited")
+        console.log("visited",api)
+
+        let newUi = [...ui,api]
+        setUi(newUi)
+
+        
+        
+    }
     return (
         <div>
 
-            <h1 className="ml-[20px]">Total Country : {api.length}</h1>
+            <h1 className="text-center">Total Country : {api.length}</h1>
+            <h1 className="text-center">Visited Country : {ui.length}</h1>
+
+            <ul className="text-center">
+                {
+                    ui.map((e,index) => <li key={index} className="">{e.name.common}</li>)
+                }
+            </ul>
             <div className="grid grid-cols-3 gap-5">
             {
-                api.map(e => <CardCountry key={e.cca3} e={e}></CardCountry>)
+                api.map(e => <CardCountry ui={ui} visited = {visited} key={e.cca3} e={e}></CardCountry>)
             }
             </div>
 
