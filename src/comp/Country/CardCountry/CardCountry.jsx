2d8e30e7d17d88f5078ee09233beb6a766bea85f
Sakib3602 <sakibsarker6969@gmail.com>
2024-03-10 23:03:55 +0600

    added conditional css and make card using api

--- src/comp/Country/CardCountry/CardCountry.jsx
+++ src/comp/Country/CardCountry/CardCountry.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 
 
-const CardCountry = ({e}) => {
+const CardCountry = ({e:{name , capital, coatOfArms, cca3}}) => {
 
    
-    console.log(e)
+    
+
+    // let {name , capital} = e
+
+    let [visit,setVisit] = useState(false)
+
+    let clickk = ()=>{
+        setVisit(!visit)
+    }
+
+
 
     
     return (
-        <div>
-            <h1>Name: {e.name.common}</h1>
+       <div className="">
+         <div className={`${visit ? "bg-red-300" : ''}  border-[2px] border-solid border-[red] w-[480px] min-h-[400px] m-3 p-[10px] rounded-2xl`}>
+            <img className="h-[120px]" src={coatOfArms.png} alt="" />
+            <h1 className="text-[60px]">Name: {name?.common}</h1>
+            <h1 className="text-[30px]">Capital : {capital}</h1>
+            <p>Code : {cca3}</p>
+
+            <button onClick={clickk} className="border-[2px] border-solid border-[red] p-2 rounded-lg">Visited</button>
+            {visit ? 'I have Visited This Place.' : 'I want to visit'}
             
         </div>
+       </div>
     );
 };
 
--- src/comp/Country/Country.jsx
+++ src/comp/Country/Country.jsx
@@ -12,13 +12,16 @@ const Country = () => {
         .then(res => res.json())
         .then(data => setApi(data))
     },[])
+    console.log(api)
     return (
         <div>
 
-            <h1>{api.length}</h1>
+            <h1 className="ml-[20px]">Total Country : {api.length}</h1>
+            <div className="grid grid-cols-3 gap-5">
             {
                 api.map(e => <CardCountry key={e.cca3} e={e}></CardCountry>)
             }
+            </div>
 
 
 
